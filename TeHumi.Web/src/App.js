b9ce90635704359b2836d738d49912695f084c66
Wilkin Vásquez <20131954@itla.edu.do>
2019-07-13 12:03:23 -0400

    Added UI.

--- TeHumi.Web/src/App.js
+++ TeHumi.Web/src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import SocketIOClient from 'socket.io-client';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      response: {
+        temperature: 0,
+        humidity: 0
+      },
+      endPoint: 'https://tehumi-socket-io.herokuapp.com'
+    }
+  }
+
+  componentDidMount() {
+    const endPoint = this.state.endPoint;
+    const socket = SocketIOClient(endPoint);
+
+    socket.on('FromAPI', data => {
+      this.setState({
+        response: data
+      });
+    });
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+        <div id="main-container" >
+          <div id="temperature-container" className='section-container' >
+            <div className="section-title" >
+              Temperatura
+            </div>
+
+            <div className="section-value" >
+              { this.state.response.temperature } °C
+            </div>
+          </div>
+
+          <div id="humidity-container" className='section-container' >
+            <div className="section-title">
+              Humedad
+            </div>
+
+            <div className="section-value" >
+              { this.state.response.humidity } %
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
+    )
+  }
 }
 
 export default App;
